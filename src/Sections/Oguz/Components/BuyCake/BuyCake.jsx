82a19b915554d80a43a964530bd32e0db99a28b9
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-28 16:58:37 +0300

    Buy Cake Section

---
+++ src/Sections/Oguz/Components/BuyCake/BuyCake.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import BuyCakeButton from "../BuyCakeButton/BuyCakeButton";
+import Learn from "../Learn/Learn";
+import Stats from "../Stats/Stats";
+import "./BuyCake.scss";
+const BuyCake = () => {
+  return (
+    <div className="buy-cake">
+      <h1 className="buy-cake__title">
+        <span>CAKE</span> makes our world go round.
+      </h1>
+      <p className="buy-cake__description">
+        CAKE token is at the heart of the PancakeSwap ecosystem. Buy it, win it,
+        farm it, spend it, stake it... heck, you can even vote with it!
+      </p>
+      <div className="buy-cake__interact">
+        <BuyCakeButton />
+        <Learn />
+      </div>
+      <Stats />
+    </div>
+  );
+};
+
+export default BuyCake;
