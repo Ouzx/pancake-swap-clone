126409622c55f007d24b2edd4db458756c5bacd9
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-28 16:58:08 +0300

    Learn

---
+++ src/Sections/Oguz/Components/Learn/Learn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { RxExternalLink } from "react-icons/rx";
+import "./Learn.scss";
+const Learn = () => {
+  return (
+    <div className="learn">
+      <span>Learn </span>
+      <RxExternalLink className="learn__icon" />
+    </div>
+  );
+};
+
+export default Learn;
