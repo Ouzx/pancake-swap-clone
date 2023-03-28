861092afa8943f6df4b997a2e79f566580f9c3b6
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-28 16:58:22 +0300

    Buy Cake Button

---
+++ src/Sections/Oguz/Components/BuyCakeButton/BuyCakeButton.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./BuyCakeButton.scss";
+const BuyCakeButton = () => {
+  return <button className="buy-cake-button">Buy CAKE</button>;
+};
+
+export default BuyCakeButton;
