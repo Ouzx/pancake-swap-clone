f830eafdf78b91613c3ac6007c7581f87a8625fb
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-29 19:57:49 +0300

    Button component upgrade

--- src/Sections/Oguz/Components/Button/Button.jsx
+++ src/Sections/Oguz/Components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Button.scss";
-const Button = ({ text, style }) => {
+const Button = ({ text, style, children }) => {
   return (
     <button style={style} className="general-button">
       {text}
+      {children}
     </button>
   );
 };
