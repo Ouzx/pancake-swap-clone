d9ae61919e7f07d053657f76dbd5f9bcdfe50ea3
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-29 22:03:35 +0300

    footer container responsive

--- src/Sections/Oguz/Components/Footer/Footer.jsx
+++ src/Sections/Oguz/Components/Footer/Footer.jsx
@@ -14,28 +14,30 @@ import SocialIcons from "../SocialIcons/SocialIcons";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <Logo />
-      <Seperator />
+    <div className="footer">
+      <div className="footer-container">
+        <Logo />
+        <Seperator />
 
-      <div className="footer-container__coin-button">
-        <PsCoin />
-        <Button text="Buy CAKE" style={{ padding: "0.3rem 1rem" }}>
-          <AiOutlineArrowRight />
-        </Button>
+        <div className="footer-container__coin-button">
+          <PsCoin />
+          <Button text="Buy CAKE" style={{ padding: "0.3rem 1rem" }}>
+            <AiOutlineArrowRight />
+          </Button>
+        </div>
+
+        <div className="footer-container__theme-language">
+          <ThemeSwitch />
+          <LanguageSelector />
+        </div>
+
+        <Seperator />
+
+        <Links />
+        <Seperator visible={false} topGap="42px" />
+        <SocialIcons />
+        <Seperator topGap="42px" />
       </div>
-
-      <div className="footer-container__theme-language">
-        <ThemeSwitch />
-        <LanguageSelector />
-      </div>
-
-      <Seperator />
-
-      <Links />
-      <Seperator visible={false} topGap="42px" />
-      <SocialIcons />
-      <Seperator topGap="42px" />
     </div>
   );
 };
