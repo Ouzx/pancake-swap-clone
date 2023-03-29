f6f9c71e28ae01db80bf3f3617d863fe7d9bc9ee
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-29 16:37:29 +0300

    footer section added

--- src/Sections/Oguz/index.jsx
+++ src/Sections/Oguz/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import BuyCake from "./Components/BuyCake/BuyCake";
 import ConnectWallet from "./Components/Connect Wallet/ConnectWallet";
+import Footer from "./Components/Footer/Footer";
 const index = () => {
   return (
     <>
       <BuyCake />
       <ConnectWallet />
+      <Footer />
     </>
   );
 };
