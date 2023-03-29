facc2882eebd87483862d0cddcbba4d47caa90e3
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-29 23:25:07 +0300

    back to top button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.scss";
+
+import BackToTop from "./Components/BackToTop/BackToTop";
+
 import OguzSection from "./Sections/Oguz";
 function App() {
   return (
     <div className="App">
       <OguzSection />
+      <BackToTop />
     </div>
   );
 }
