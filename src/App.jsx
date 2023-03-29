5e4c0b4268fe5f14c08313a21fe6fc3e4dc06668
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-29 23:54:17 +0300

    dark mode applied

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import "./App.scss";
 import { ThemeProvider } from "./Context/ThemeContext";
 
 import BackToTop from "./Components/BackToTop/BackToTop";
-
+import DarkMode from "./Components/DarkMode";
 import OguzSection from "./Sections/Oguz";
 function App() {
   return (
     <ThemeProvider>
       <div className="App">
         <OguzSection />
+        <DarkMode />
         <BackToTop />
       </div>
     </ThemeProvider>
