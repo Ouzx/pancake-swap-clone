711b15188a9ad12f93b21670abeb8a0b0b8450df
Ouzx <oguzhankandakoglu@gmail.com>
2023-03-29 17:15:00 +0300

    seperator

--- src/Sections/Oguz/Components/Footer/Footer.jsx
+++ src/Sections/Oguz/Components/Footer/Footer.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Footer.scss";
 
 import Logo from "../Logo/Logo";
+import Seperator from "../Seperator/Seperator";
 import Links from "../Links/Links";
 
 const Footer = () => {
   return (
     <div className="footer-container">
       <Logo />
+      <Seperator />
       <Links />
     </div>
   );
---
+++ src/Sections/Oguz/Components/Seperator/Seperator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Seperator.scss";
+const Seperator = () => {
+  return <div className="seperator"></div>;
+};
+
+export default Seperator;
