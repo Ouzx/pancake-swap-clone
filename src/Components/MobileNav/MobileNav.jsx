5e79613f06d8b57df25ed820d78a44a64f16a215
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-26 16:04:42 +0300

    extra styling for mobile nav

--- src/Components/MobileNav/MobileNav.jsx
+++ src/Components/MobileNav/MobileNav.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MobileNav.scss";
 import { ReactComponent as Trade } from "../../assets/icons/trade.svg";
 import { ReactComponent as Earn } from "../../assets/icons/earn.svg";
@@ -9,25 +9,25 @@ import { ReactComponent as More } from "../../assets/icons/more.svg";
 const MobileNav = () => {
   return (
     <div className="mobile-nav">
-      <div className="mobile-nav__item">
+      <button className="mobile-nav__item">
         <Trade />
         Trade
-      </div>
-      <div className="mobile-nav__item">
+      </button>
+      <button className="mobile-nav__item">
         <Earn />
         Earn
-      </div>
-      <div className="mobile-nav__item">
+      </button>
+      <button className="mobile-nav__item">
         <Win />
         Win
-      </div>
-      <div className="mobile-nav__item">
+      </button>
+      <button className="mobile-nav__item">
         <Nft />
         NFT
-      </div>
-      <div className="mobile-nav__item">
+      </button>
+      <button className="mobile-nav__item">
         <More />
-      </div>
+      </button>
     </div>
   );
 };
