95faa1adc1204125ecc78f5fa3d7c7d9ec18cfaf
Ouzx <oguzhankandakoglu@gmail.com>
2023-04-26 14:56:20 +0300

    language selector added

--- src/Sections/Oguz/Components/LanguageSelector/LanguageSelector.jsx
+++ src/Sections/Oguz/Components/LanguageSelector/LanguageSelector.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { TbWorld } from "react-icons/tb";
-
+import List from "./List/List";
 import "./LanguageSelector.scss";
 
 const LanguageSelector = () => {
   return (
     <div className="langauge-selector">
-      <TbWorld size={24} />
-      EN
+      <div className="langauge-selector__button">
+        <TbWorld size={24} />
+        EN
+      </div>
+      <div className="langauge-selector__content">
+        <List />
+      </div>
     </div>
   );
 };
